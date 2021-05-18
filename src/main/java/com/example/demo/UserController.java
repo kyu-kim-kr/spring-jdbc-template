a429a16bf3eeca0bf98f4020b7f098234fa4e787
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-05-18 17:23:11 +1200

    Add controller

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDao userDao;
+
+    @GetMapping("/api")
+    public List<User> users() {
+        return userDao.findAll();
+    }
+}
