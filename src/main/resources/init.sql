9815a1cac9953d95e95c8273c51f5b48328e1eb2
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2021-05-18 17:23:00 +1200

    Add example data

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -5,3 +5,5 @@ CREATE TABLE user(
                        first_name VARCHAR(255) NOT NULL,
                        last_name VARCHAR(255) NOT NULL
 );
+
+INSERT INTO `user` (`first_name`,`last_name`) VALUES ("Chadwick","Rosales"),("Gil","Wise"),("Kennan","Battle"),("Cedric","Harris"),("Burton","Frederick"),("Ivor","Rowland"),("Hiram","Mccoy"),("Arthur","Cantrell"),("Elvis","Sloan"),("Mark","Warren");
